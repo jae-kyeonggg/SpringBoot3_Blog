d179ccb99dd34294b605777528bc073a485edbf1
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-07-01 03:16:42 +0900

    ~Test

--- src/main/java/me/hanjaekyeong/springbootdeveloper/TestController.java
+++ src/main/java/me/hanjaekyeong/springbootdeveloper/TestController.java
@@ -1,13 +1,20 @@
 package me.hanjaekyeong.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    TestService testservice;
+
     @GetMapping("/test")
-    public String test() {
-        return "Hello World";
+    public List<Member> getAllMembers() {
+        List<Member> members = testservice.getAllMembers();
+        return members;
     }
 }
