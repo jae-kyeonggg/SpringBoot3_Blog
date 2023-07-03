0d3fc47632989ca44ae91e96cf66cddbf1552b6b
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-07-03 12:00:46 +0900

    add gitignore

--- README.md
+++
@@ -1,4 +0,0 @@
-### 스프링 부트 3 백엔드 개발자 되기
-https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=316907362
-
-https://github.com/shinsunyoung/springboot-developer
--- src/main/java/me/hanjaekyeong/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.hanjaekyeong.springbootdeveloper.repository;
-
-import me.hanjaekyeong.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
